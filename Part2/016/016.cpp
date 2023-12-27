7bfe1755acdebaa3e43e9d075fee0a4b57494109
HongKyoungIn <ghdruddls999@naver.com>
2023-12-27 23:50:14 +0900

    2023-12-27 Study : 기본 문법 이해하기

---
+++ Part2/016/016.cpp
@@ -0,0 +1,20 @@
+﻿// 016. 문자형 변수 이해하기(char)
+
+#include "pch.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+    char ch1 = 'c';
+    char ch2 = 200;
+
+    unsigned char ch3 = 'c';
+    unsigned char ch4 = 200;
+
+    printf("char ch1 = %c, %d\n", ch1, ch1);
+    printf("char ch2 = %c, %d\n", ch2, ch2);
+    printf("char ch3 = %c, %d\n", ch3, ch3);
+    printf("char ch4 = %c, %d\n", ch4, ch4);
+
+    return 0;
+}
---
+++ Part2/018/018.cpp
@@ -0,0 +1,15 @@
+﻿// 
+
+#include "pch.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+    double a = 333.333333;
+    cout.precision(6);
+    cout << std::fixed;
+
+    cout << a;
+
+    return 0;
+}
