af87a9966165338772694c081812daf281f312eb
HongKyoungIn <ghdruddls999@naver.com>
2024-01-03 23:39:42 +0900

    2024-01-03 Study : 반복문을 이용한 구구단 15단 출력하기
    내가 만든 코드와 책에 나와있는 코드의 차이점 분석

--- Part2/040/040.cpp
+++ Part2/040/040.cpp
@@ -5,7 +5,15 @@
 using namespace std;
 
 int main() {
-    
+    int num = 0;
+
+    cin >> num;
+
+    for(int i = 1; i < num + 1; i++) {
+        for(int j = 1; j < 10; j++) {
+            cout << i << " * " << j << " = " << i * j << endl;
+        }
+    }
 
     return 0;
 }
