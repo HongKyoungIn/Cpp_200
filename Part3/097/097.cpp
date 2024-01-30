49d9df2cc0b606155b59eebafe0aa2b2d66fb2f7
HongKyoungIn <ghdruddls999@naver.com>
2024-01-30 20:38:07 +0900

    097. 프로그램 실행 폴더 알아내기(current_path)

---
+++ Part3/097/097.cpp
@@ -0,0 +1,15 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <filesystem>
+
+using namespace std;
+namespace fs = std::filesystem;
+
+int main() {
+	cout << "프로젝트 폴더 : " << fs::current_path() << endl;
+
+	return 0;
+}
+/*
+프로젝트 -> 프로젝트 속성 -> 구성 속성 -> C/C++ -> 언어 -> C++ 언어 표준 -> ISO C++17
+*/
