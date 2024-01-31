f99f4e5939f774bbeb83463d2a35ce7b9d2e7be6
HongKyoungIn <ghdruddls999@naver.com>
2024-01-31 22:26:52 +0900

    103. 파일 크기 확인하기(file_size)

---
+++ Part3/103/103.cpp
@@ -0,0 +1,16 @@
+﻿// 103. 파일 크기 확인하기(file_size)
+
+#include "pch.h"
+#include <iostream>
+#include <filesystem>
+
+using namespace std;
+namespace fs = std::filesystem;
+
+int main() {
+	int size = fs::file_size("연개수영 전설.txt");
+
+	cout << "파일 크기 : " << size << "바이트" << endl;
+
+	return 0;
+}
