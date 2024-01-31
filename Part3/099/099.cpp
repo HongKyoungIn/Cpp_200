77b437dba8b0537d858fa91e53e793ccce6040bf
HongKyoungIn <ghdruddls999@naver.com>
2024-01-31 22:18:39 +0900

    099. 하위 폴더 목록 확인하기(directory_iterator)

---
+++ Part3/099/099.cpp
@@ -0,0 +1,18 @@
+﻿// 099. 하위 폴더 목록 확인하기(directory_iterator)
+
+#include "pch.h"
+#include <iostream>
+#include <string>
+#include <filesystem>
+
+using namespace std;
+namespace fs = std::filesystem;
+
+int main() {
+	string directory = "C:\\Program Files";
+
+	for(auto &name : fs::directory_iterator(directory)) // auto : 자료형을 미리 정해놓지 않고 커파일 단계에서 정해준다.
+		std::cout << name << '\n';
+
+	return 0;
+}
