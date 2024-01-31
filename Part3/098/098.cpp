a82d29f1ccaf563ef19f1fc11065b49d7ce896d8
HongKyoungIn <ghdruddls999@naver.com>
2024-01-31 22:16:20 +0900

    098. 폴더 존재 여부 확인하기(exists)

--- Part3/098/098.cpp
+++ Part3/098/098.cpp
@@ -1,12 +1,17 @@
-﻿// 
+﻿// 098. 폴더 존재 여부 확인하기(exists)
 
 #include "pch.h"
 #include <iostream>
+#include <filesystem>
 
 using namespace std;
+namespace fs = std::filesystem;
 
 int main() {
-    
+	if(fs::exists("c:\\Target") == true)
+		cout << "폴더가 존재합니다" << endl;
+	else
+		cout << "폴더가 없습니다" << endl;
 
     return 0;
 }
