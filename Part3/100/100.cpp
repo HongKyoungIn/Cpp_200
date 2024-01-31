4bc70fc2ddfb7e23dd1f31cb5072b430c411c1e8
HongKyoungIn <ghdruddls999@naver.com>
2024-01-31 22:20:29 +0900

    100. 파일 생성, 복사, 삭제하기(filesystem)

---
+++ Part3/100/100.cpp
@@ -0,0 +1,17 @@
+﻿// 100. 파일 생성, 복사, 삭제하기(filesystem)
+
+#include "pch.h"
+#include <iostream>
+#include <filesystem>
+
+using namespace std;
+namespace fs = std::filesystem;
+
+int main() {
+	fs::create_directory("temp");
+	fs::copy("temp", "temp_copy");
+	fs::remove("temp");
+	fs::remove("temp_copy");
+
+    return 0;
+}
