c65f921a7ea721bd0ef4b4d4bf21622d2d2d1a34
HongKyoungIn <ghdruddls999@naver.com>
2024-01-31 22:22:26 +0900

    101. 파일 복사 삭제하기(copy, remove)

---
+++ Part3/101/101.cpp
@@ -0,0 +1,15 @@
+﻿// 101. 파일 복사 삭제하기(copy, remove)
+
+#include "pch.h"
+#include <iostream>
+#include <filesystem>
+
+using namespace std;
+namespace fs = std::filesystem;
+
+int main() {
+	fs::copy("연개수영 전설.txt", "(복사)연개수영 전설.txt");
+	fs::remove("연개수영 전설.txt");
+
+    return 0;
+}
