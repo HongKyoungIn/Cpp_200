5bfdaeae66126b3c60205915000596182e42deb3
HongKyoungIn <ghdruddls999@naver.com>
2024-01-31 22:24:59 +0900

    102. 파일 존재 여부 확인하기(good)

---
+++ Part3/102/102.cpp
@@ -0,0 +1,22 @@
+﻿// 102. 파일 존재 여부 확인하기(good)
+
+#include "pch.h"
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+namespace fs = std::filesystem;
+
+int main() {
+	ifstream stream;
+	stream.open("C:\\BOOTNXT");
+
+	if(stream.good() == true)
+		cout << "파일이 존재합니다" << endl;
+	else
+		cout << "파일이 없습니다" << endl;
+
+	stream.close();
+
+	return 0;
+}
