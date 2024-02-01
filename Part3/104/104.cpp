3cce128882d6329e2d59d6184e4763fd4612580d
HongKyoungIn <ghdruddls999@naver.com>
2024-02-01 20:03:07 +0900

    104. 클래스 정의하기(class)

---
+++ Part3/104/104.cpp
@@ -0,0 +1,35 @@
+﻿// 104. 
+
+#include "pch.h"
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class KingInfo {
+public:
+	void SetName(string name) {
+		name_ = name;
+	}
+
+	string GetName() const {
+		return name_;
+	}
+
+private:
+	string name_;
+};
+
+int main() {
+	KingInfo king_info;
+	king_info.SetName("조선 세조 이유");
+
+	cout << king_info.GetName() << endl;
+
+	return 0;
+}
+/*
+Public : 공개된 정보, 외부 접근 허용
+Protected : 제한된 정보, 해당 클래스나 서브 클래스에서만 접근 허용
+Private : 제한된 정보, 해당 클래스에서만 접근 허용
+*/
