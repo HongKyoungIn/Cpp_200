e5be261c98e915ff6686a8278194adae146494a8
HongKyoungIn <ghdruddls999@naver.com>
2024-02-05 19:50:25 +0900

    115. 클래스 상속 ① Has-A 관계

---
+++ Part3/115/115.cpp
@@ -0,0 +1,31 @@
+﻿// 115. 클래스 상속 ① Has-A 관계
+
+#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+class Building1 {
+public:
+	void Name() {
+		cout << "노른자 위 좋은 건물" << endl;
+	}
+};
+
+class BuildingOwner : public Building1 {
+public:
+	void MyBuilding() {
+		cout << "내 보물 : ";
+		Name();
+	}
+};
+
+int main(void) {
+	BuildingOwner envious;
+	envious.MyBuilding();
+
+	return 0;
+}
+/*
+한 클래스가 다른 클래스를 포함하는 상속 관계.
+*/
