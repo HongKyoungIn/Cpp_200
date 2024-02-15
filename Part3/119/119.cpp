d2499abcee98b250f7b923f79b2dc1e87d4d1ba4
HongKyoungIn <ghdruddls999@naver.com>
2024-02-15 22:46:04 +0900

    119. Friend 함수 사용하기

--- Part3/119/119.cpp
+++ Part3/119/119.cpp
@@ -1,12 +1,30 @@
-﻿// 
+﻿// 119. Friend 함수 사용하기
 
 #include "pch.h"
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-int main() {
-    
+class Ondal {
+public:
+	friend void GetYear(Ondal ondal) {
+		cout << "아단성 전투 연도 : " << ondal.year_ << endl;
+	}
 
-    return 0;
+	void SetYear(int year) {
+		year_ = year;
+	}
+
+private:
+	int year_;
+};
+
+int main() {
+	Ondal ondal;
+	ondal.SetYear(590);
+
+	GetYear(ondal);
+
+	return 0;
 }
