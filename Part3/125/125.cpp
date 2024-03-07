f11793417072a0908f01194077980dc00cd81108
HongKyoungIn <ghdruddls999@naver.com>
2024-03-07 15:46:55 +0900

    125. type_traits 사용하기 ③ (conditional)

--- Part3/125/125.cpp
+++ Part3/125/125.cpp
@@ -2,11 +2,24 @@
 
 #include "pch.h"
 #include <iostream>
+#include <type_traits>
+#include <string>
 
 using namespace std;
 
-int main() {
+template <typename T1, typename T2>
+auto TmpFunc(T1 x, T2 y) {
+    using type = typename conditional<is_same<T1, T2>::value, int, double>::type;
+    return type{};
+}
 
+int main() {
+    auto value = TmpFunc(1, 22.3);
+
+    if(is_same<decltype(value), double>::value)
+        cout << "double 자료형입니다." << endl;
+    else
+        cout << "double 자료형이 아닙니다." << endl;
 
     return 0;
 }
