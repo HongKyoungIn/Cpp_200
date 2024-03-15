41bdae50442c925806a90251927c0dad1a6ddf05
HongKyoungIn <ghdruddls999@naver.com>
2024-03-15 19:27:33 +0900

    128. 가변인자 템플릿(typename... T)

---
+++ Part3/128/128.cpp
@@ -0,0 +1,26 @@
+﻿// 128. 가변인자 템플릿 사용하기(typename... T)
+#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+void Type1(string msg, int no, double value) {
+    cout << msg << ", 에러 번호 " << no << ", 오류값 " << value << endl;
+}
+
+void Type2(int no, double value) {
+    cout << no << ", 오류값 " << value << endl;
+}
+
+template<typename... T>
+void PrintLog(T... arg0) {
+    // Type1(arg0...);
+    Type2(arg0...);
+}
+
+int main() {
+    // PrintLog<string, int, double>("Warnig", 100, 22.5);
+    PrintLog<int, double>(101, 55.6);
+
+    return 0;
+}
---
+++ Part3/128_2/128_2.cpp
@@ -0,0 +1,31 @@
+﻿// 128. 가변인자 템플릿 예제2
+
+#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+template<typename T>
+auto print(T arg) {
+    cout << arg << endl;
+}
+
+template<typename T, typename... Types>
+void print(T arg, Types... args) {
+    cout << arg;
+    print(args...);
+}
+
+// C++14에서의 사용 예시
+auto add(int a, int b) {
+    return a + b;
+}
+
+
+int main() {
+    print(5); // 1번 print를 호출
+    print(1, 2, 3, 4, 5); // 2번 프린트를 호출
+    print(1, 2, 3); // 2번 프린트를 호출
+
+    return 0;
+}
