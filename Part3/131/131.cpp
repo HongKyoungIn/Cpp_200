8eab24070cb448b27e131372baa52049ce38f59d
HongKyoungIn <ghdruddls999@naver.com>
2024-03-19 14:13:08 +0900

    131. 예외 처리 이해하기(try catch exception)

---
+++ Part3/131/131.cpp
@@ -0,0 +1,21 @@
+﻿// 131. 예외 처리 이해하기(try catch exception)
+
+#include "pch.h"
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    vector<int> data;
+    int number = 0;
+
+    try {
+        number = data.at(10);
+    }
+    catch(exception e) {
+        cout << e.what() << endl;
+    }
+
+    return 0;
+}
